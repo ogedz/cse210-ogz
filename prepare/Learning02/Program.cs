ba0e748522177d799f67fc431933629e7cf876b3
ogedz <ogedengbe_m@yahoo.com>
2024-01-17 09:47:31 +0100

    Changed to Display() as the method name

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
+
+        // Display jobs within a resume
+        Resume myResume = new Resume("Allison Rose");
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+        myResume.Display();
     }
 }
