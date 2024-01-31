9f5d2b03d3926918b1367d29931340955d893516
ogedz <ogedengbe_m@yahoo.com>
2024-01-31 21:34:18 +0100

    Changed to Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -3,7 +3,23 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    {   
+        // Base Assignment object
+        Assignment Assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(Assignment.GetSummary());
+
+        Console.WriteLine();
+
+        // MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+        // WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
